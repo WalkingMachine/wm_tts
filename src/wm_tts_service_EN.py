24df5cd7c2743ad2a7e619c64380bf9db1b02c18
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2017-08-24 15:34:00 -0400

    Add topic to tts system

--- src/wm_tts_service_EN.py
+++ src/wm_tts_service_EN.py
@@ -6,12 +6,14 @@ from std_msgs.msg import String
 from wm_tts.srv import *
 import os
 from subprocess import check_call, CalledProcessError
+from wm_tts.msg import *
+
 
 class wm_tts:
     def __init__(self, node_name):
         rospy.init_node(node_name)
         s = rospy.Service('wm_say', say_service, self.say)
-
+        sub = rospy.Subscriber('say', say, self.callback)
     def say(self, req):
         rospy.loginfo(req.say.sentence)
 
@@ -25,6 +27,18 @@ class wm_tts:
         except CalledProcessError:
             rospy.logwarn('Last subprocess call was not valid.')
             return False
+
+    def callback(self, data):
+        rospy.loginfo(data.sentence)
+
+        try:
+            os.system("pico2wave -l=en-US -w=/tmp/test.wav " + '"'+str(data.sentence)+'"')
+            os.system("aplay /tmp/test.wav")
+            os.system("rm /tmp/test.wav")
+            rospy.loginfo("SARA said: %s", data.sentence)
+
+        except CalledProcessError:
+            rospy.logwarn('Last subprocess call was not valid.')
 
 if __name__ == '__main__':
 
