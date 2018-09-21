6a0652c69d4c7221864e1d66b888c7554856128f
philippe <philippe117@live.fr>
2018-09-20 23:23:24 -0400

    implémente Mary et met des paramètres potables.

--- src/wm_tts_service.py
+++ src/wm_tts_service.py
@@ -11,6 +11,24 @@ from wm_tts.msg import say
 from wm_tts.srv import say_service
 
 
+# HTTP + URL packages
+import httplib2
+
+#from urlparse import urlparse
+#from urllib.parse import urlencode, quote # For URL creation
+import urllib
+
+
+# To play wave files
+import pygame
+import math # For ceiling
+
+
+# Mary server informations
+mary_host = "localhost"
+mary_port = "59125"
+
+
 class wm_tts:
 
     def __init__(self, node_name):
@@ -38,6 +56,10 @@ class wm_tts:
         self.gain = rospy.get_param("/gain", 8)
         self.forceOffline = rospy.get_param("/force_offline", False)
 
+        print("langue = "+str(self.langue))
+        print("gain = "+str(self.gain))
+        print("force_offline = "+str(self.forceOffline))
+
         # Choisi si on utilise le tts online ou offline
         if not self.forceOffline and self.internet_on():
             return self.online_tts(sentence)
@@ -55,7 +77,10 @@ class wm_tts:
 
 
     def offline_tts(self, sentence):
-        return self.p2w_tts(sentence)
+        if not self.mary_tts(sentence):
+            return self.p2w_tts(sentence)
+        else:
+            return True
 
     # Utilise le Pico2Wav pour dire quelque chose.
     def p2w_tts(self, sentence):
@@ -76,6 +101,56 @@ class wm_tts:
             rospy.logwarn('Last subprocess call was not valid.')
         return False
 
+    # Utilise le Mary tts pour dire quelque chose.
+    def mary_tts(self, sentence):
+
+        if not "en-US" in self.langue:
+            print( "not in english")
+            return False
+
+        try:
+            # Build the query
+            # "VOICE":"cmu-slt-hsmm", # Voice informations  (need to be compatible)
+            query_hash = {"INPUT_TEXT": sentence,
+                          "INPUT_TYPE": "TEXT",  # Input text
+                          "LOCALE": "en_US",
+                          "Voice": "cmu-slt-hsmm",
+                          "style": "",
+                          "OUTPUT_TYPE": "AUDIO",
+                          "AUDIO": "WAVE",  # Audio informations (need both)
+                          "effect_F0Add_selected": "on",
+                          "effect_F0Add_parameters": "40.0",
+                          "effect_F0Scale_selected": "on",
+                          "effect_F0Scale_parameters": "0.5"
+                          }
+            query = urllib.urlencode(query_hash)
+            print("query = \"http://%s:%s/process?%s\"" % (mary_host, mary_port, query))
+
+            # Run the query to mary http server
+            h_mary = httplib2.Http()
+            resp, content = h_mary.request("http://%s:%s/process?" % (mary_host, mary_port), "POST", query)
+
+            #  Decode the wav file or raise an exception if no wav files
+            if (resp["content-type"] == "audio/x-wav"):
+
+                # Write the wav file
+                f = open("/tmp/output_wav.wav", "wb")
+                f.write(content)
+                f.close()
+
+                # Play the wav file
+                # os.system("aplay /tmp/output_wav.wav")
+                pygame.mixer.init(frequency=48000, )  # Initialise the mixer
+                s = pygame.mixer.Sound("/tmp/output_wav.wav")
+                s.play()
+                pygame.time.wait(int(math.ceil(s.get_length() * 1000)))
+
+            else:
+                raise Exception(content)
+            return True
+        except CalledProcessError:
+            rospy.logwarn('Last subprocess call was not valid.')
+        return False
 
     # Utilise le GoogleSpeechAPI pour dire quelque chose.
     def online_tts(self, sentence):
